6f00eadef13a3ad238abbd88c0a9d3a62fe6485b
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-05 14:01:04 +0100

    feat: set up main site navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FunctionExpressionTesterComponent } from './function-expression-tester/function-expression-tester.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, FunctionExpressionTesterComponent],
+    imports: [RouterOutlet, FunctionExpressionTesterComponent,MatToolbarModule, MatIconModule, MatButtonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
