a19e38f1a3c766ffd2a934dd87dbc38a3472fb9d
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-11 12:24:51 +0100

    feat: add Details page for HousingLocations

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
@@ -8,5 +11,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DetailsComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common/';
 
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId: number;
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
 }
