ef984abaf991cb9e84f9f3a2b5454cfc98295ad5
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-12 19:37:08 +0100

    feat: add apply-form to Details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,9 +18,25 @@ export class DetailsComponent {
   housingLocationId: number;
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  
+  formBuilder = inject(FormBuilder);
+  
+  applyForm = this.formBuilder.group({
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+  });
 
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
+
+  submitApplication(): void {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
