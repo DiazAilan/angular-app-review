b16b7195c9c00c390ce4b25a74dac5e0f800d78f
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-13 09:42:05 +0100

    feat: implement housing filter by city

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,8 +14,21 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingService: HousingService = inject(HousingService);
   housingLocations: HousingLocation[] = [];
+  filteredLocations: HousingLocation[] = [];
 
   constructor() {
     this.housingLocations = this.housingService.getAllHousingLocations();
+    this.filteredLocations = this.housingLocations;
+  }
+
+  filterResults(query: string): void {
+    if (!query) {
+      this.filteredLocations = this.housingLocations;
+      return;
+    }
+
+    this.filteredLocations = this.housingLocations.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(query.toLowerCase()),
+    );
   }
 }
